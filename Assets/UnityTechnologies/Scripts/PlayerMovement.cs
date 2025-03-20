3a6ba15dd265e7d5a78e65f908d38cffe0b8482f
AnshikkaSaini <110725511+AnshikkaSaini@users.noreply.github.com>
2025-03-20 12:26:29 +0000

    Fix: Player Movement

--- Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector3 m_Movement;
     public float turnSpeed = 20f;
-    Quaternion m_Rotation = Quaternion.identity;
-    Rigidbody m_Rigidbody;
+
     Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
 
     void Start()
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
     }
+
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Set(- horizontal, 0f, - vertical);
         m_Movement.Normalize();
 
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
@@ -29,14 +31,20 @@ public class PlayerMovement : MonoBehaviour
         m_Animator.SetBool("IsWalking", isWalking);
 
         Vector3 desiredForward = Vector3.RotateTowards
-                                 (transform.forward, m_Movement,
-                                 turnSpeed * Time.deltaTime, 0f);
+                                (transform.forward,
+                                m_Movement, turnSpeed *
+                                Time.deltaTime, 0f);
+
         m_Rotation = Quaternion.LookRotation(desiredForward);
     }
-   void OnAnimatorMove()
+
+    void OnAnimatorMove()
     {
-        m_Rigidbody.MovePosition(m_Rigidbody.position
-            + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MovePosition(m_Rigidbody.position 
+            + transform.forward * m_Animator.deltaPosition.magnitude);
+
+
         m_Rigidbody.MoveRotation(m_Rotation);
+        m_Animator.applyRootMotion = false;
     }
 }
